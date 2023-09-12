a7200c101acb3696b453525ec78a7c0e174aaa7c
Luciano Holanda Porto <lhporto1@stefanini.com>
2023-09-12 10:31:41 -0300

    Teste de elementos na página

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,14 +1,44 @@
 /// <reference types="cypress" />
 
-describe('cadastro', ()=> {
-it('deve cadastrar um novo usuário', ()=> {
-    cy.visit('index.html')
+import {faker} from '@faker-js/faker'
 
-    cy.get('#name').type('João da Silva')
-    cy.get('input[placeholder="joao.silva@email.com"]').type('joao@email.com')
-    cy.get('#password').type('senha12345678');
-    
-    cy.contains('button', "Cadastrar").click()
-})
+describe('Teste QA - Cadastro de Usuários', ()=> {
+
+    it('Visitar a página e verificar campos', ()=> {
+        cy.visit('index.html')
+        cy.title().should('be.equal', 'Cadastro de Usuários')
+        cy.get('h1').should('have.text', 'Cadastro de usuários')
+        cy.get('p').should('have.text', 'Para realizar o cadastro de um usuário, insira dados válidos no formulário e acione a opção Cadastrar :)')
+        cy.get(':nth-child(2) > label').should('have.text', 'Nome')
+        cy.get('#name').should('be.visible')
+        cy.get(':nth-child(3) > label').should('have.text', 'E-mail')
+        cy.get('#email').should('be.visible')
+        cy.get(':nth-child(4) > label').should('have.text', 'Senha')
+        cy.get('#password').should('be.visible')
+        cy.get('button')
+            .should('be.visible')
+            .should('contain', 'Cadastrar')
+            cy.get('#userTableWrapper').should('not.be.visible')
+    }),
+
+    it('Deve cadastrar um novo usuário com sucesso', ()=> {
+        cy.visit('index.html')
+
+        const randomName = faker.person.fullName();
+        const randomEmail = faker.internet.email();
+        const randomPassword = faker.internet.password();
+
+        cy.get('#name').type(randomName)
+        cy.get('input[placeholder="joao.silva@email.com"]').type(randomEmail)
+        cy.get('#password').type(randomPassword);
+
+        cy.contains('button', "Cadastrar").click()
+
+        cy.get('#userTableWrapper').should('be.visible')
+
+        cy.get('tbody tr').should('have.length', 1)
+        cy.get('tbody tr td').eq(1).should('contain', randomName)
+        cy.get('tbody tr td').eq(2).should('contain', randomEmail)
+    })
 
 })
